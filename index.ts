e443660795812fdf790999d13a536115ae679beb
toluOjo <ojotolulope137@gmail.com>
2023-10-12 17:02:30 +0100

    create cached endpoint

--- index.ts
+++ index.ts
@@ -4,10 +4,15 @@ import redis from "redis";
 
 const app = express();
 
-const redisClient = redis.createClient({
-  host: "localhost",
-  port: 6379,
-});
+let redisClient;
+
+(async () => {
+    redisClient = redis.createClient();
+  
+    redisClient.on("error", (error) => console.error(`Error : ${error}`));
+  
+    await redisClient.connect();
+  })();
 
 app.use(express.json());
 
@@ -16,15 +21,36 @@ app.get("/", (req, res) => {
 });
 
 
-app.get("/users", async(req, res) => {
+app.get("/cached-users", async(req, res) => {
 try {
-    const users = await axios.get("https://jsonplaceholder.typicode.com/users")
-    return res.status(200).json({ message: "Welcome!" });
+const cacheKey = "users";
+let isCached;
+let results;
+const cacheResults = await redisClient.get(cacheKey);
+if (cacheResults) {
+  isCached = true;
+  results = JSON.parse(cacheResults);
+} else {
+  results = await axios.get("https://jsonplaceholder.typicode.com/users");
+  if (results.length === 0) {
+    throw "API returned an empty array";
+  }
+  await redisClient.set(cacheKey, JSON.stringify(results));
+}
+
+res.send({
+  fromCache: isCached,
+  data: results,
+});
 
 } catch (error) {
     res.status(500).json({Error:"Internal Server Error " + error.message});
 } 
  });
+
+ app.get("/users",()=>{
+    
+ })
     
  const PORT = process.env.PORT || 3000;
  app.listen(PORT, () => {
