4cdc194f214498a1b66448ef37bf303f52f9c72c
toluOjo <ojotolulope137@gmail.com>
2023-10-13 00:32:05 +0100

    connect to redis

--- index.ts
+++ index.ts
@@ -1,18 +1,18 @@
 import axios from "axios";
 import express from "express";
-import redis from "redis";
+import * as redis from "redis";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
-let redisClient;
+const redisUrl: string ="redis://localhost:6379";
 
-(async () => {
-    redisClient = redis.createClient();
-  
-    redisClient.on("error", (error) => console.error(`Error : ${error}`));
-  
-    await redisClient.connect();
-  })();
+const redisClient = redis.createClient({
+  url: redisUrl,
+});
+
+redisClient.on("error", (error: any) => console.error(`Error : ${error}`));
 
 app.use(express.json());
 
@@ -20,39 +20,44 @@ app.get("/", (req, res) => {
   return res.status(200).json({ message: "Welcome!" });
 });
 
+app.get("/cached-users", async (req, res) => {
+  try {
+    const cacheKey = "users";
+    let isCached = false;
+    let results: any;
+    const cacheResults = await redisClient.get(cacheKey);
+    if (cacheResults) {
+      isCached = true;
+      results = JSON.parse(cacheResults);
+    } else {
+      results = await axios.get("https://jsonplaceholder.typicode.com/users");
+      if (results.data.length === 0) {
+        throw "API returned an empty array";
+      }
+      await redisClient.set(cacheKey, JSON.stringify(results.data));
+    }
 
-app.get("/cached-users", async(req, res) => {
-try {
-const cacheKey = "users";
-let isCached;
-let results;
-const cacheResults = await redisClient.get(cacheKey);
-if (cacheResults) {
-  isCached = true;
-  results = JSON.parse(cacheResults);
-} else {
-  results = await axios.get("https://jsonplaceholder.typicode.com/users");
-  if (results.length === 0) {
-    throw "API returned an empty array";
+    res.status(200).json({
+      fromCache: isCached,
+      users: results,
+    });
+  } catch (error: any) {
+    res.status(500).json({ Error: "Internal Server Error " + error.message });
   }
-  await redisClient.set(cacheKey, JSON.stringify(results));
-}
-
-res.send({
-  fromCache: isCached,
-  data: results,
 });
 
-} catch (error) {
-    res.status(500).json({Error:"Internal Server Error " + error.message});
-} 
- });
+app.get("/users", async (req, res) => {
+  const users = await axios.get("https://jsonplaceholder.typicode.com/users");
 
- app.get("/users",()=>{
-    
- })
-    
- const PORT = process.env.PORT || 3000;
- app.listen(PORT, () => {
-   console.log(`Server is running on port ${PORT}`);
- });
+  res.status(200).json({
+    data: users.data,
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, async () => {
+  await redisClient.connect()
+  .then(() => 
+  console.log("redis connected ..."));
+  console.log(`Server is running on port ${PORT}`);
+});
